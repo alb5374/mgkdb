90fc6ebd620c0eeb37fa78e78017f1645e99efc6
Austin Blackmon <ablack@utexas.edu>
2018-10-01 15:47:19 -0500

    finds if run is linear automatically before uploading

--- mgk_uploader.py
+++ mgk_uploader.py
@@ -14,6 +14,7 @@ Optional fields:    confidence
 """
 
 from mgk_file_handling import *
+from ParIO import *
 import os
 
 ########################################################################
@@ -22,38 +23,37 @@ user = 'A. Blackmon'
 
 output_folder = '.'
 multiple_runs = True
-linear = True   ######## CHECK PARAMS FILE INSTEAD ########
+#linear = True   ######## CHECK PARAMS FILE INSTEAD ########
 
 if not multiple_runs:
     confidence = '5'  ### 1-10, 1: little confidence, 10: well checked ###
 else:
     confidence = 'None'  ### Set if same for all runs ###
     
-if linear:
-    lin = 'linear'
-else:
-    lin = 'nonlin'
-    
+#if linear:
+#    lin = 'linear'
+#else:
+#    lin = 'nonlin'
+#    
 input_heat = 'None'
     
 ### enter any relevant keywords, i.e., ETG, ITG, pedestal, core ###
-keywords = 'ETG, pedestal, GENE, ' + lin
+keywords = 'ETG, pedestal, GENE, ' #+ lin
 
 #######################################################################
 
 if multiple_runs:
     folder_list = []
     for dirpath, dirnames, files in os.walk(output_folder):
+        
         for count, name in enumerate(dirnames, start=0):
-            if linear:
-                if name.find('linear') != -1:
-                    folder = os.path.join(name)
-                    folder_list.append(folder)
-            else:
-                if name.find('nonlin') != -1:
-                    folder = os.path.join(name)
-                    folder_list.append(folder)
-    for folder in folder_list:
-        upload_to_mongo(folder, user, linear, confidence, input_heat, keywords)
+
+            folder = os.path.join(name)
+            folder_list.append(folder)
+            
+            linear = isLinear(name)
+                  
+            for folder in folder_list:
+                upload_to_mongo(folder, user, linear, confidence, input_heat, keywords)
 else:
     upload_to_mongo(output_folder, user, linear, confidence, input_heat, keywords)
